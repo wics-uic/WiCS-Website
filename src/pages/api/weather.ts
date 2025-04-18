15ba1a56146c724f71a5ceb2896777942e8b3188
michelle <michellemt0518@gmail.com>
2025-04-18 18:16:06 -0500

    distributing weather-api files to proper location

--- src/pages/api/weather.ts
+++ src/pages/api/weather.ts
@@ -1,22 +1,56 @@
-import type { APIRoute } from "astro";
-const API_KEY = import.meta.env.WEATHER_API_KEY;
-const API_URL = import.meta.env.WEATHER_API_URL;
+// import type { APIRoute } from "astro";
+// const API_KEY = import.meta.env.WEATHER_API_KEY;
+// const API_URL = import.meta.env.WEATHER_API_URL;
+const API_KEY = "315162c22a7656116cbad3b7a28fcf25";
+const API_URL = "https://api.openweathermap.org/data/2.5/weather?units=metric";
 
+    
 async function checkWeather(city) {
-    const response = await fetch(`${API_URL}&units=metric&appid=${API_KEY}&q=${city}`);
+    const response = await fetch(`${API_URL}&appid=${API_KEY}&q=${city}`);
 
-    if (response.status == 404) {
+    if (!response.ok) {
         // display Chicago 
+        console.error("City not found or an API error occurred.");
+        return;
     }
-    else {
-        var data = await response.json();
-    }
+    var data = await response.json();
     console.log(data);
+    
 
-    // data from json file 
-    document.querySelector(".location")?.innerHTML = data.name;
-    document.querySelector(".date")?.innerHTML = data.;
-    document.querySelector(".temp")?.innerHTML = Math.round(data.main.temp) + "°C";
+    // update weather location 
+    document.querySelector(".location").innerHTML = `${data.name}, ${data.sys.country}`;
+    document.querySelector(".date").innerHTML = new Date().toLocaleDateString('en-US', {weekday: 'long'}); // grab current date
+    
+    // celsius temperature
+    const tempCelsius = Math.round(data.main.temp);
+    document.querySelector(".degree-cel").innerHTML = `${tempCelsius}°C`;
+
+    // convert to fahrenheit
+    const tempF = Math.round((tempCelsius * (9/5)) + 32);
+    document.querySelector(".degree-fahre").innerHTML = `${tempF} F`;
+
+    // update weather description and icon
+    document.querySelector(".description").innerHTML = data.weather[0].description.charAt(0).toUpperCase() + data.weather[0].description.slice(1);
+    const weatherIcon = document.querySelector(".weather-img");
+    
+    // set icon 
+    const weatherCondition = data.weather[0].main.toLowerCase();
+
+    // change weather icon
+    if(weatherCondition.includes("clouds")) {
+        weatherIcon.src = "src/assets/weather-images/clouds.png";
+    }
+    else if(weatherCondition.includes("clear")) {
+        weatherIcon.src = "src/assets/weather-images/clear.png";
+    }
+    else if(weatherCondition.includes("rain")) {
+        weatherIcon.src = "src/assets/weather-images/rain.png";
+    }
+    else if(weatherCondition.includes("drizzle")) {
+        weatherIcon.src = "src/assets/weather-images/drizzle.png";
+    }
+    else if(weatherCondition.includes("mist")) {
+        weatherIcon.src = "src/assets/weather-images/mist.png";
+    }
     
 }
-
