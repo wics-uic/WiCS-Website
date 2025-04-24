c58fa557acb2d45db762c5f13fb6b3d4ddf76779
Nandana <nandanasheri@gmail.com>
2025-04-24 15:01:48 -0500

    calendar api works and fetches events to the frontend

--- src/pages/api/calendar.ts
+++ src/pages/api/calendar.ts
@@ -23,12 +23,13 @@ export const GET: APIRoute = async () => {
     }
 
     const data = await response.json();
+
     return new Response(JSON.stringify(data), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
   } catch (error) {
-    return new Response(JSON.stringify({ error: error.message }), {
+    return new Response(JSON.stringify({ error: error }), {
       status: 500,
       headers: { "Content-Type": "application/json" },
     });
