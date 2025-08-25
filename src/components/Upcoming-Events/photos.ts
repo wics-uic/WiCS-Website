d97beb19d323d7db3d86ec652cdbe0014b99b389
cheetodustflori <florianneche@gmail.com>
2025-08-25 16:53:17 -0500

    updating paths for upcoming events

---
+++ src/components/Upcoming-Events/photos.ts
@@ -0,0 +1,19 @@
+import img84 from '../../assets/events/84.png?url';
+import acm from '../../assets/events/acm.png?url';
+import akuna from '../../assets/events/akuna.png?url';
+import btt from '../../assets/events/btt.png?url';
+import cna from '../../assets/events/cna.png?url';
+import dev from '../../assets/events/dev.png?url';
+import epsilonvisit from '../../assets/events/epsilonvisit.png?url';
+import hackgap from '../../assets/events/hackgap.png?url';
+import insideScoop from '../../assets/events/inside-scoop.png?url';
+import mentorship from '../../assets/events/mentorship.png?url';
+import movieNight from '../../assets/events/movie-night.png?url';
+import picnicSocial from '../../assets/events/picnic-social.png?url';
+import spookyBakeSale from '../../assets/events/spooky-bake-sale.png?url';
+import studySocial from '../../assets/events/study-social.png?url';
+
+export default [
+  img84, acm, akuna, btt, cna, dev, epsilonvisit, hackgap,
+  insideScoop, mentorship, movieNight, picnicSocial, spookyBakeSale, studySocial
+];
